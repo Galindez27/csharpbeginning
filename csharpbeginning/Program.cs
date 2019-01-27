757eb1f6f09b94d22ad8d528d2be6f0778930e5a
Jonathan Galindez <galindez@bu.edu>
2019-01-27 02:17:09 -0500

    Add project files.

---
+++ csharpbeginning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharpbeginning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
